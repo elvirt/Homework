9b0e3859b871fc58baa941cd9a4457ba2193138c
Liubomyr Virt <elvirtlubko@gmail.com>
2019-03-03 17:32:08 +0200

    uploading hw

---
+++ task04_Collections2_BinTree/Actionable.java
@@ -0,0 +1,4 @@
+@FunctionalInterface
+public interface Actionable {
+    void run();
+}
---
+++ task04_Collections2_BinTree/DescriptionAction.java
@@ -0,0 +1,18 @@
+public class DescriptionAction {
+    String description;
+    Actionable action;
+
+    public DescriptionAction(String description, Actionable action) {
+        this.description = description;
+        this.action = action;
+
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Actionable getAction() {
+        return action;
+    }
+}
